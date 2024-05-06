e9894ad89d54f2f2068a199591677e5945dc1fc0
shofinurdin <shofinurdin@gmail.com>
2024-05-06 11:30:24 +0700

    'pertama'

--- home.py
+++ home.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Titanic ML",
 html_temp = """
 		<div style="background-color:#3872fb;padding:5px;border-radius:10px">
 		<h3 style="color:white;text-align:center;font-family:arial;">Titanic Machine Learning </h3>
-		<h3 style="color:white;text-align:center;font-family:arial;">PJJ DAS II 2023</h3>
+		<h3 style="color:white;text-align:center;font-family:arial;">PJJ DAS I 2024</h3>
 		</div>
 		"""
 
